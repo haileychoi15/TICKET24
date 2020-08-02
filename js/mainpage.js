f636c9bbe3d6cda679b3dec17003872e5cda605d
yuyeong choi <imdud0612@gmail.com>
2020-08-02 23:06:40 +0900

    mainpage promotion slider

--- js/mainpage.js
+++ js/mainpage.js
@@ -2,8 +2,51 @@ window.addEventListener('DOMContentLoaded', () => {
 
     styleRanking();
     styleCategory();
+    slidePromotion();
 
 });
+
+function slidePromotion() {
+
+    let promotionGroup = document.querySelector('.promotion-group');
+    let promotionItem = promotionGroup.querySelectorAll('.promotion-item');
+
+    let totalQty = promotionItem.length;
+    let cloneQty = 3;
+
+    // 화면에 직접적으로 보이는 아이템 개수만큼 클론노드 만들기
+    for(let i=0; i<cloneQty; i++){
+        let itemClone = promotionItem[i].cloneNode(true);
+        promotionGroup.insertAdjacentElement('beforeend', itemClone);
+    }
+
+    // 아이템 하나의 너비를 얻어 와서 아이템 그룹의 너비를 만들기
+    let itemStyle = window.getComputedStyle(promotionItem[0]);
+    let width = Number(itemStyle.width.replace("px",""));
+    let marginLeft = Number(itemStyle.marginLeft.replace("px",""));
+    let marginRight = Number(itemStyle.marginRight.replace("px",""));
+
+    width = width + marginLeft + marginRight;
+    promotionGroup.style.width = (width * (totalQty + cloneQty)) + 'px';
+
+    let count = 0;
+    setInterval(() => {
+
+        count++;
+        promotionGroup.style.transition = 'transform 250ms ease-in-out';
+        promotionGroup.style.transform = 'translate3d(-'+ (width * count) +'px, 0px, 0px)';
+
+        if(count === totalQty){
+            setTimeout(() => {
+
+                promotionGroup.style.transition = '0ms';
+                promotionGroup.style.transform = 'translate3d(0px, 0px, 0px)';
+
+            },201);
+            count = 0;
+        }
+    },2000);
+}
 
 function styleRanking() {
 
