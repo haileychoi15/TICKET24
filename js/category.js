f1023b3e17e6ef65510d3de18dd0805acae3a861
yuyeong choi <imdud0612@gmail.com>
2020-08-10 05:50:09 +0900

    category page button style

--- js/category.js
+++ js/category.js
@@ -1,24 +1,20 @@
 window.addEventListener('DOMContentLoaded', () => {
 
-    let subCategoryButton = document.querySelector('.sub-category > button');
+    let subCategoryButton = document.querySelector('.category-box-button');
     subCategoryButton.addEventListener('click', (event) => {
 
         let box = document.querySelector('.sub-category-box');
-        let icon = document.querySelector('.sub-category .button-icon');
-        icon.style.transform = 'rotate(180deg)';
 
         if(box.classList.contains('show')){
 
-            console.log(icon);
-
             box.style.display = 'none';
             box.classList.replace('show','hide');
-            icon.style.transform = 'rotate(0)';
+            event.currentTarget.classList.remove('on'); // 클릭 화살표 방향 바꾸기
         }
         else{
             box.style.display = 'flex';
             box.classList.replace('hide','show');
-            icon.style.transform = 'rotate(180deg)';
+            event.currentTarget.classList.add('on'); // 클릭 화살표 방향 바꾸기
 
         }
 
