0e7a3554d35a3a2895713d80eea9a3caf2219029
yuyeong choi <imdud0612@gmail.com>
2020-08-09 22:40:56 +0900

    join page validate form

--- js/join.js
+++ js/join.js
@@ -45,7 +45,85 @@ window.addEventListener('DOMContentLoaded',() => {
 
     });
 
+    // 틀려서 정보 다시 입력할 때 빨간 색 없애기
+    let form = document.querySelector('.join-form');
+    form.addEventListener('focusin', (event) => {
+
+        let target = event.target;
+        if(target.nodeName === 'INPUT'){
+
+            console.log(target.nodeName, target.classList);
+
+            let inputs = form.querySelectorAll('input');
+            inputs.forEach((item) => {
+
+                item.classList.remove('wrong');
+            });
+        }
+    });
+
+
 });
+
+function check(re, input, message) {
+    if(re.test(input.value)) {
+        input.classList.remove('wrong');
+        return true;
+    }
+    input.classList.add('wrong');
+    console.log(input.value, message); // 확인용
+    //input.focus();
+    //return false;
+}
+
+
+// 유효성 검사
+function validateForm() {
+
+    let reID = /^[a-zA-Z0-9]{6,12}$/; // 아이디 정규식
+    let rePW = /^[a-zA-Z0-9]{8,20}$/; // 패스워드 정규식
+    let reEmail = /^[0-9a-zA-Z]([-_.]?[0-9a-zA-Z])*@[0-9a-zA-Z]([-_.]?[0-9a-zA-Z])*.[a-zA-Z]{2,3}$/i; //이메일 정규식
+    let reMobile = /^[0-9]{3,4}$/; // 모바일 정규식
+
+    let userid = document.querySelector('#userid');
+    let passwd = document.querySelector('#userpw');
+    let name = document.querySelector('#name');
+    let email = document.querySelector('#email');
+    let mobile = document.querySelectorAll('.mobile');
+
+    if(!check(reID,userid,"아이디는 6~12자의 영문과 숫자로만 입력")) {
+        return false;
+    }
+
+    if(!check(rePW,passwd,"비밀번호는 8~20자의 영문과 숫자로만 입력")) {
+        return false;
+    }
+
+    if(name.value.trim() === ''){
+        name.classList.add('wrong');
+        return false;
+    }
+
+    if(!check(reEmail,email,"이메일형식이 올바르지 않습니다.")) {
+        return false;
+    }
+
+    if(!check(reMobile,mobile[0],"휴대폰 번호 형식이 올바르지 않습니다.")) {
+        return false;
+    }
+
+    if(!check(reMobile,mobile[1],"휴대폰 번호 형식이 올바르지 않습니다.")) {
+        return false;
+    }
+
+    if(!check(reMobile,mobile[2],"휴대폰 번호 형식이 올바르지 않습니다.")) {
+        return false;
+    }
+
+    alert('회원가입이 완료되었습니다.');
+    return true;
+
+}
 
 function CheckEveryBox(target, agreeAllBox, agreeButton) {
 
