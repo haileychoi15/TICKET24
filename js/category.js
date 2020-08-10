d80a3af1e9f78e56f16e579091bb72ed2012e7c8
yuyeong choi <imdud0612@gmail.com>
2020-08-10 14:24:38 +0900

    category page style

--- js/category.js
+++ js/category.js
@@ -1,5 +1,6 @@
 window.addEventListener('DOMContentLoaded', () => {
 
+    // 카테고리 박스 눌렀을 때 이벤트
     let subCategoryButton = document.querySelector('.category-box-button');
     subCategoryButton.addEventListener('click', (event) => {
 
@@ -15,12 +16,30 @@ window.addEventListener('DOMContentLoaded', () => {
             box.style.display = 'flex';
             box.classList.replace('hide','show');
             event.currentTarget.classList.add('on'); // 클릭 화살표 방향 바꾸기
-
         }
 
     });
 
-    // 카테고리 누를 때 이벤트 발생
+
+    // 박스안에 서브 카테고리 눌렀을 때 이벤트
+    let box = document.querySelector('.sub-category-box'); // 위에도 있음
+    box.addEventListener('click', (event) => {
+
+        let target = event.target;
+        if(target.nodeName === 'P'){ // 버튼 역할하는 태그
+
+            console.log(target);
+
+            let subCategory = document.querySelector('.category-box-button > span');
+            console.log(target.innerText, subCategory.innerText);
+            subCategory.innerText = target.innerText;
+        }
+
+
+    });
+
+
+    // 오더 버튼 누를 때 이벤트 발생
     let orderGroup = document.querySelector('.order-group');
     orderGroup.addEventListener('click', (event) => {
 
